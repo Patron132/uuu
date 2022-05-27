d20308c20aae778ac4f3ed2b3214756771486cf7
Patron132 <106367924+Patron132@users.noreply.github.com>
2022-05-27 19:54:43 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,12 +30,11 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [PATRON]
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ JOIN : [Click Me](https://t.me/+SpjYH3U1_ZE3OTE1)
 """
